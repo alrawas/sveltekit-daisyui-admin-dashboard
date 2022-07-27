d88ecf3fa5bde79d0c4ec6a942bc126cb8876acc
alrawas <alrawas.abed@gmail.com>
2022-07-27 17:39:21 +0300

    added navabar on pc and drawer on mobile. layout set up

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,39 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<slot />
+<div class="drawer">
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<Navbar />
+		<!-- Page content here -->
+		<slot />
+		<Footer />
+	</div>
+  <!-- Sidebar on mobile here -->
+	<div class="drawer-side">
+		<label for="my-drawer-3" class="drawer-overlay" />
+		<ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+			<!-- Sidebar content here -->
+			<li>
+				<a
+					on:click={() => {
+						document.getElementById('my-drawer-3').click();
+					}}
+					href="/">Home</a
+				>
+			</li>
+			<li>
+				<a
+					on:click={() => {
+						document.getElementById('my-drawer-3').click();
+					}}
+					href="/about">About</a
+				>
+			</li>
+		</ul>
+	</div>
+</div>
